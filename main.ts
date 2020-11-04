df0a31f3839c663aaa124d5457fdd7a82a43c6d9
yunchi0825 <71751221+yunchi0825@users.noreply.github.com>
2020-11-04 16:06:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.plot(4 - x, y)
+            led.plot(4 - y, x)
+        }
+        basic.pause(100)
+    }
 })
